682eb5e2e678b3eff346803facd01df319a9e4d1
SzymiYay <szymoon09@gmail.com>
2025-08-28 01:07:17 +0200

    fix: snapshots and remove unneccesary tests

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -3,13 +3,6 @@ import { test, expect } from '@playwright/test'
 test('homepage shows heading and table', async ({ page }) => {
   await page.goto('/')
 
-  await expect(
-    page.getByRole('heading', { name: /ETF Momentum/i })
-  ).toBeVisible()
-
-  const table = page.locator('#results-table')
-  await expect(table).toBeVisible()
-
   await expect(page.getByRole('cell', { name: 'ETF' })).toBeVisible()
   await expect(
     page.getByRole('cell', { name: '12M Momentum (%)' })
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,7 @@
 import App from './App'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 describe('App component', () => {
-  test('renders heading', () => {
-    render(<App />)
-    expect(
-      screen.getByRole('heading', { name: /ETF Momentum/i })
-    ).toBeInTheDocument()
-  })
-
   test('App matches snapshot', () => {
     const { asFragment } = render(<App />)
     expect(asFragment()).toMatchSnapshot()
